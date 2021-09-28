632dd9da9f884195f4ce049fb012de8710e79392
tugolukov <tugolukov40@ya.ru>
2021-09-28 23:39:19 +0300

    Initial commit

---
+++ LearningSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
